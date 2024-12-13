a61a536327cf2e21e5f4c7908012983f01f5b225
Samra Shafiq <samrashafiq82@gmail.com>
2024-12-14 00:10:28 +0500

    Updated the project with new features

--- src/app/FAQPage/page.tsx
+++ src/app/FAQPage/page.tsx
@@ -60,11 +60,11 @@ const FAQPage: React.FC = () => {
 
        {/* Centered Text */}
        <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center px-4">
-         <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold">404 Error</h1>
+         <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold">FAQ Page</h1>
          <p className="text-sm md:text-base lg:text-lg mt-2 flex items-center gap-2">
            <span className="text-white">Home</span>
            <IoChevronForwardOutline />
-           <span className="text-[#FF9F0D]">  404</span>
+           <span className="text-[#FF9F0D]"> Faq</span>
          </p>
        </div>
      </div>
